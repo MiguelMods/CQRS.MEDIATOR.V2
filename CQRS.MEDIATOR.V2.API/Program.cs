2b3f4043cc84eecdf0e22e069d3ee28c16e13223
MiguelMods <miguelmodd@gmail.com>
2025-05-11 10:37:14 -0400

    agregando nuevos servicios a inyectar

--- CQRS.MEDIATOR.V2.API/Program.cs
+++ CQRS.MEDIATOR.V2.API/Program.cs
@@ -1,4 +1,8 @@
 using CQRS.MEDIATOR.V2.API.DataContext;
+using CQRS.MEDIATOR.V2.API.Repositories.Contracs;
+using CQRS.MEDIATOR.V2.API.Repositories.Implementations;
+using CQRS.MEDIATOR.V2.API.Services.Contract;
+using CQRS.MEDIATOR.V2.API.Services.Implementations;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +15,10 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<ApplicationDataBaseContext>(opt => { 
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IStatusService, StatusService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
