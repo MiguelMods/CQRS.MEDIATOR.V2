daf1236d57a16755ff473ff15f82c65eb3cb8143
MiguelMods <miguelmodd@gmail.com>
2025-05-11 08:39:34 -0400

    adding dbContext class to manage ef core connections and entiries

---
+++ CQRS.MEDIATOR.V2.API/DataContext/ApplicationDataBaseContext.cs
@@ -0,0 +1,11 @@
+﻿using CQRS.MEDIATOR.V2.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRS.MEDIATOR.V2.API.DataContext
+{
+    public class ApplicationDataBaseContext(DbContextOptions<ApplicationDataBaseContext> options) : DbContext(options)
+    {
+        public DbSet<Status> Statuses { get; set; }
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
