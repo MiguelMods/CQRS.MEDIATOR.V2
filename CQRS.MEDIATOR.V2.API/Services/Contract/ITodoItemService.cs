238e6125fcea48e137347e5b4f2c29f9be6ebfca
MiguelMods <miguelmodd@gmail.com>
2025-05-11 12:25:59 -0400

    agregando nuevo servicio para traer los servicios

---
+++ CQRS.MEDIATOR.V2.API/Services/Contract/ITodoItemService.cs
@@ -0,0 +1,9 @@
+﻿using CQRS.MEDIATOR.V2.API.Entities;
+
+namespace CQRS.MEDIATOR.V2.API.Services.Contract
+{
+    public interface ITodoItemService : IServices<TodoItem>
+    {
+
+    }
+}
---
+++ CQRS.MEDIATOR.V2.API/Services/Implementations/TodoItemService.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq.Expressions;
+using CQRS.MEDIATOR.V2.API.Entities;
+using CQRS.MEDIATOR.V2.API.Repositories.Contracs;
+using CQRS.MEDIATOR.V2.API.Services.Contract;
+
+namespace CQRS.MEDIATOR.V2.API.Services.Implementations
+{
+    public class TodoItemService(IUnitOfWork unitOfWork) : ITodoItemService
+    {
+        private IUnitOfWork UnitOfWork { get; } = unitOfWork;
+        private IGenericRepository<TodoItem> TodoItemRepository { get; } = unitOfWork.GetRepository<TodoItem>();
+
+        public async Task<IEnumerable<TodoItem>> GetAllAsync()
+            => await TodoItemRepository.GetAllAsync();
+
+        public async Task<TodoItem> GetByAsync(Expression<Func<TodoItem, bool>> expression)
+            => await TodoItemRepository.GetByAsync(expression);
+
+        public async Task<TodoItem> AddAsync(TodoItem entity)
+        {
+            var todoItem = await TodoItemRepository.AddAsync(entity);
+            var result = await UnitOfWork.SaveChangesAsync();
+
+            if (result)
+                return todoItem;
+
+            throw new Exception("Error while saving changes");
+        }
+
+        public async Task<TodoItem> UpdateAsync(TodoItem entity)
+        {
+            var todoItem = await TodoItemRepository.GetByAsync(x => x.TodoItemId == entity.TodoItemId) ?? throw new Exception("TodoItem not found");
+
+            todoItem.Title = entity.Title;
+            todoItem.Description = entity.Description;
+            todoItem.IsCompleted = entity.IsCompleted;
+            todoItem.StartDate = entity.StartDate;
+            todoItem.EndDate = entity.EndDate;
+
+            if (todoItem.IsCompleted)
+            {
+                todoItem.EndDate = todoItem.EndDate == null ? DateTime.Now : todoItem.EndDate;
+            }
+
+            var result = await TodoItemRepository.UpdateAsync(todoItem);
+            var saveResult = await UnitOfWork.SaveChangesAsync();
+
+            if (saveResult)
+                return todoItem;
+
+            throw new Exception("Error while updated changes");
+        }
+
+        public async Task<bool> DeleteByAsync(Expression<Func<TodoItem, bool>> expression)
+        {
+            var todoItem = await TodoItemRepository.GetByAsync(expression) ?? throw new Exception("TodoItem not found");
+            
+            var result = await TodoItemRepository.DeleteByAsync(x => x.TodoItemId == todoItem.TodoItemId);
+            var saveResult = await UnitOfWork.SaveChangesAsync();
+
+            if (saveResult)
+                return result;
+
+            throw new Exception("Error while deleting changes");
+        }
+    }
+}
