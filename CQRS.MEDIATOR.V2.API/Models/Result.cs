01f1a2c7f262c0ac2706088099ff4f0cfdd0499d
MiguelMods <miguelmodd@gmail.com>
2025-05-11 12:06:36 -0400

    adding result class

---
+++ CQRS.MEDIATOR.V2.API/Models/Result.cs
@@ -0,0 +1,37 @@
+﻿namespace CQRS.MEDIATOR.V2.API.Models
+{
+    public class Result<Type>
+    {
+        public bool IsSuccess { get; set; }
+        public Type? Data { get; set; }
+        public string? Message { get; set; }
+        public static Result<Type> Success(Type data)
+        {
+            return new Result<Type>
+            {
+                IsSuccess = true,
+                Data = data,
+                Message = string.Empty
+            };
+        }
+        public static Result<bool> Failure(string? message)
+        {
+            return new Result<bool>
+            {
+                IsSuccess = false,
+                Data = default,
+                Message = message
+            };
+        }
+
+        public static Result<Type> Failure(Type data, string? message)
+        {
+            return new Result<Type>
+            {
+                IsSuccess = false,
+                Data = data,
+                Message = message
+            };
+        }
+    }
+}
