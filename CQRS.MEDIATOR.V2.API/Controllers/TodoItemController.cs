aa0f969686de6439978c9e312341a3d4a2c315b4
MiguelMods <miguelmodd@gmail.com>
2025-05-11 23:11:29 -0400

    agrando patron CQRS

--- CQRS.MEDIATOR.V2.API/Controllers/TodoItemController.cs
+++ CQRS.MEDIATOR.V2.API/Controllers/TodoItemController.cs
@@ -1,135 +1,118 @@
 ﻿using CQRS.MEDIATOR.V2.API.Entities;
 using CQRS.MEDIATOR.V2.API.Models;
 using CQRS.MEDIATOR.V2.API.Models.Request;
-using CQRS.MEDIATOR.V2.API.Services.Contract;
-using Microsoft.AspNetCore.Http;
+using CQRS.MEDIATOR.V2.API.QuerysAndCommands;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CQRS.MEDIATOR.V2.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class TodoItemController(ITodoItemService todoItemService) : ControllerBase
+    public class TodoItemController(IMediator mediator) : ControllerBase
     {
         [HttpGet("{includeStatus:bool?}")]
         public async Task<IActionResult> Get(bool includeStatus = true)
         {
-            var result = includeStatus
-                ? await todoItemService.GetAllWithStatusAsync()
-                : await todoItemService.GetAllAsync();
+            var result = await mediator.Send(new GetAllTodoITemsQuery(includeStatus));
 
             if (result == null)
                 return NotFound(Result<IEnumerable<TodoItem>>.Failure([], "data not avaible"));
 
-            return Ok(Result<IEnumerable<TodoItem>>.Success(result));
+            return Ok(result);
         }
 
         [HttpGet("{id:long}/{includeStatus:bool?}")]
         public async Task<IActionResult> Get(long id, bool includeStatus = true)
         {
-            var result = includeStatus
-                ? await todoItemService.GetByIdWithStatusAsync(id)
-                : await todoItemService.GetByAsync(x => x.TodoItemId == id);
-
+            var result = await mediator.Send(new GetTodoItemByIdQuery(id, includeStatus));
             if (result == null)
                 return NotFound(Result<TodoItem>.Failure("data not avaible"));
-
-            return Ok(Result<TodoItem>.Success(result));
+            return Ok(result);
         }
 
         [HttpGet("status/{statusId:long}")]
         public async Task<IActionResult> GetByStatusId(long statusId)
         {
-            var result = await todoItemService.GetAllByStatusIdAsync(statusId);
+            var result = await mediator.Send(new GetAllTodoItemsByStatusIdQuery(statusId));
+
             if (result == null)
                 return NotFound(Result<IEnumerable<TodoItem>>.Failure("data not avaible"));
-            return Ok(Result<IEnumerable<TodoItem>>.Success(result));
-        }
 
-        [HttpPut("{id:long}/status/{statusid:long}")]
-        public async Task<IActionResult> UpdateTodoStatus(long id, long statusId)
-        {
-            var result = await todoItemService.UpdateTodoStatusAsync(id, statusId);
-
-            if (result == false)
-                return NotFound(Result<bool>.Failure("data not avaible"));
-
-            return Ok(Result<bool>.Success(true));
+            return Ok(result);
         }
 
         [HttpPost("")]
         public async Task<IActionResult> Post([FromBody] TodoItemCreateRequest request)
         {
-            var result = await todoItemService.AddAsync(new TodoItem
-            {
-                Title = request.Title,
-                Description = request.Description,
-                IsCompleted = request.IsCompleted,
-                StartDate = request.StartDate,
-                EndDate = request.EndDate,
-            });
+            var result = await mediator.Send(new CreateTodoItemCommand(request.Title, request.Description, request.IsCompleted, request.StartDate, request.EndDate));
 
             if (result == null)
                 return NotFound(Result<TodoItem>.Failure("data not avaible"));
 
-            return CreatedAtAction(nameof(Get), new { id = result.TodoItemId }, Result<TodoItem>.Success(result));
+            if (!result.IsSuccess)
+                return BadRequest(result);
+
+            return CreatedAtAction(nameof(Get), new { id = result.Data?.TodoItemId }, result);
         }
 
         [HttpPut("")]
         public async Task<IActionResult> Put([FromBody] TodoItemUpdateRequest request)
         {
-            var result = await todoItemService.UpdateAsync(new TodoItem
-            {
-                TodoItemId = request.TodoItemId,
-                Title = request.Title,
-                Description = request.Description,
-                IsCompleted = request.IsCompleted,
-                StartDate = request.StartDate,
-                EndDate = request.EndDate,
-            });
+            var result = await mediator.Send(new UpdateTodoItemCommand(request.TodoItemId, request.Title, request.Description, request.IsCompleted, request.StartDate, request.EndDate));
 
             if (result == null)
                 return NotFound(Result<TodoItem>.Failure("data not avaible"));
 
-            return Ok(Result<TodoItem>.Success(result));
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
+        }
+
+        [HttpPut("{id:long}/status/{statusid:long}")]
+        public async Task<IActionResult> Put(long id, long statusId)
+        {
+            var result = await mediator.Send(new UpdateTodoItemStatusCommand(id, statusId));
+            return result.IsSuccess switch
+            {
+                true => Ok(result),
+                false => NotFound(result),
+            };
         }
 
         [HttpPut("date")]
         public async Task<IActionResult> UpdateTodoDate([FromBody] TodoItemUpdateDateRequest request)
         {
-            var result = await todoItemService.UpdateTodoDateAsync(new TodoItem
+            var result = await mediator.Send(new UpdateTodoItemDateCommand(request.TodoItemId, request.StartDate, request.EndDate));
+            return result.IsSuccess switch
             {
-                TodoItemId = request.TodoItemId,
-                StartDate = request.StartDate,
-                EndDate = request.EndDate,
-            });
-
-            if (result == false)
-                return NotFound(Result<bool>.Failure("data not avaible"));
-
-            return Ok(Result<bool>.Success(true));
+                true => Ok(result),
+                false => BadRequest(result),
+            };
         }
 
         [HttpDelete("{id:long}/{delete:bool?}")]
         public async Task<IActionResult> Delete(long id, bool delete = false)
         {
-            var result = delete
-                ? await todoItemService.DeleteRegisterAsync(id)
-                : await todoItemService.DeleteByAsync(x => x.TodoItemId == id);
+            var result = await mediator.Send(new DeleteTodoItemCommand(id, delete));
 
-            if (result == false)
+            if (result == null)
                 return NotFound(Result<bool>.Failure("data not avaible"));
 
-            return Ok(Result<bool>.Success(true));
+            return result.IsSuccess switch
+            {
+                true => Ok(result),
+                false => BadRequest(result),
+            };
         }
 
         [HttpPut("recover/{id:long}")]
         public async Task<IActionResult> Recover(long id)
         {
-            var result = await todoItemService.RecoverDeleteAsync(id);
-            if (result == false)
+            var result = await mediator.Send(new UpdateRecoverTodoItemCommand(id));
+            
+            if (result == null)
                 return NotFound(Result<bool>.Failure("data not avaible"));
-            return Ok(Result<bool>.Success(true));
+            
+            return result.IsSuccess ? Ok(result) : BadRequest(result);
         }
     }
 }
