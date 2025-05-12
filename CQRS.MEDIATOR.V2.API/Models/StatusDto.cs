eaf199de74c6dd5bae4207c9c36663882fc26098
MiguelMods <miguelmodd@gmail.com>
2025-05-11 23:11:45 -0400

    agreando nuevos DTO's

---
+++ CQRS.MEDIATOR.V2.API/Models/StatusDto.cs
@@ -0,0 +1,16 @@
+﻿namespace CQRS.MEDIATOR.V2.API.Models
+{
+    public class StatusDto
+    {
+        public long StatusId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public bool Active { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string? LastUpdatedBy { get; set; }
+        public DateTime? LastUpdatedAt { get; set; }
+        public Guid? RowGuid { get; set; }
+        public bool Deleted { get; set; }
+    }
+}
---
+++ CQRS.MEDIATOR.V2.API/Models/TodoItemDto.cs
@@ -0,0 +1,23 @@
+﻿using CQRS.MEDIATOR.V2.API.Entities;
+
+namespace CQRS.MEDIATOR.V2.API.Models
+{
+    public class TodoItemDto
+    {
+        public long TodoItemId { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public long StatusId { get; set; }
+        public virtual Status Status { get; set; }
+        public bool Active { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string? LastUpdatedBy { get; set; }
+        public DateTime? LastUpdatedAt { get; set; }
+        public Guid? RowGuid { get; set; }
+        public bool Deleted { get; set; }
+    }
+}
