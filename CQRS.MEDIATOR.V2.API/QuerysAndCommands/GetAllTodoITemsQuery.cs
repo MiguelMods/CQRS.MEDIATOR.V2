864bfe04a0d38747878332e6c44c59c0f9c3f502
MiguelMods <miguelmodd@gmail.com>
2025-05-11 23:42:01 -0400

    agregando correcion tras copilar y fallar

--- CQRS.MEDIATOR.V2.API/QuerysAndCommands/GetAllTodoITemsQuery.cs
+++ CQRS.MEDIATOR.V2.API/QuerysAndCommands/GetAllTodoITemsQuery.cs
@@ -1,5 +1,4 @@
-﻿using CQRS.MEDIATOR.V2.API.Entities;
-using CQRS.MEDIATOR.V2.API.Models;
+﻿using CQRS.MEDIATOR.V2.API.Models;
 using MediatR;
 
 namespace CQRS.MEDIATOR.V2.API.QuerysAndCommands
--- CQRS.MEDIATOR.V2.API/QuerysAndCommands/GetAllTodoItemsByStatusIdQueryHandler.cs
+++ CQRS.MEDIATOR.V2.API/QuerysAndCommands/GetAllTodoItemsByStatusIdQueryHandler.cs
@@ -1,15 +1,14 @@
-﻿using CQRS.MEDIATOR.V2.API.Entities;
-using CQRS.MEDIATOR.V2.API.Models;
-using CQRS.MEDIATOR.V2.API.Repositories.Contracs;
+﻿using CQRS.MEDIATOR.V2.API.Models;
+using CQRS.MEDIATOR.V2.API.Services.Contract;
 using MediatR;
 
 namespace CQRS.MEDIATOR.V2.API.QuerysAndCommands
 {
-    public class GetAllTodoItemsByStatusIdQueryHandler(ITodoItemRepository todoItemRepository) : IRequestHandler<GetAllTodoItemsByStatusIdQuery, Result<IEnumerable<TodoItemDto>>>
+    public class GetAllTodoItemsByStatusIdQueryHandler(ITodoItemService todoItemService) : IRequestHandler<GetAllTodoItemsByStatusIdQuery, Result<IEnumerable<TodoItemDto>>>
     {
         public async Task<Result<IEnumerable<TodoItemDto>>> Handle(GetAllTodoItemsByStatusIdQuery request, CancellationToken cancellationToken)
         {
-            var result = await todoItemRepository.GetAllByStatusIdAsync(request.StatusId);
+            var result = await todoItemService.GetAllByStatusIdAsync(request.StatusId);
             var resultDto = result.Select(TodoItemDto.Map);
             return Result<IEnumerable<TodoItemDto>>.Success(resultDto);
         }
