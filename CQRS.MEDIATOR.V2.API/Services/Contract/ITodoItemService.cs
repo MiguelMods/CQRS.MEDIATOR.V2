f0042e91cf817f0291eb9f2e43c64a2274f5f434
MiguelMods <miguelmodd@gmail.com>
2025-05-11 21:31:17 -0400

    agregando nuevos servicios para todoitem

--- CQRS.MEDIATOR.V2.API/Services/Contract/ITodoItemService.cs
+++ CQRS.MEDIATOR.V2.API/Services/Contract/ITodoItemService.cs
@@ -4,6 +4,12 @@ namespace CQRS.MEDIATOR.V2.API.Services.Contract
 {
     public interface ITodoItemService : IServices<TodoItem>
     {
-
+        Task<IEnumerable<TodoItem>> GetAllWithStatusAsync();
+        Task<TodoItem> GetByIdWithStatusAsync(long id);
+        Task<IEnumerable<TodoItem>> GetAllByStatusIdAsync(long statusId);
+        Task<bool> UpdateTodoStatusAsync(long id, long statusId);
+        Task<bool> UpdateTodoDateAsync(TodoItem todoItem);
+        Task<bool> DeleteRegisterAsync(long id);
+        Task<bool> RecoverDeleteAsync(long id);
     }
 }
--- CQRS.MEDIATOR.V2.API/Services/Implementations/TodoItemService.cs
+++ CQRS.MEDIATOR.V2.API/Services/Implementations/TodoItemService.cs
@@ -13,6 +13,51 @@ namespace CQRS.MEDIATOR.V2.API.Services.Implementations
         public async Task<IEnumerable<TodoItem>> GetAllAsync()
             => await TodoItemRepository.GetAllAsync();
 
+        public async Task<IEnumerable<TodoItem>> GetAllWithStatusAsync()
+            => await UnitOfWork.TodoItemRepository.GetAllWithStatusAsync();
+
+        public async Task<TodoItem> GetByIdWithStatusAsync(long id)
+            => await UnitOfWork.TodoItemRepository.GetByIdWithStatusAsync(id);
+
+        public Task<IEnumerable<TodoItem>> GetAllByStatusIdAsync(long statusId)
+            => UnitOfWork.TodoItemRepository.GetAllByStatusIdAsync(statusId);
+
+        public async Task<bool> UpdateTodoStatusAsync(long id, long statusId)
+        {
+            var todoItem = await TodoItemRepository.GetByAsync(x => x.TodoItemId == id) ?? throw new Exception("TodoItem not found");
+
+            if (todoItem.StatusId == statusId)
+                return true;
+
+            var status = await UnitOfWork.GetRepository<Status>().GetByAsync(x => x.StatusId == statusId) ?? throw new Exception("Status not found");
+
+            todoItem.StatusId = status.StatusId;
+
+            var update = await TodoItemRepository.UpdateAsync(todoItem);
+            var result = await UnitOfWork.SaveChangesAsync();
+
+            if (result)
+                return result;
+
+            throw new Exception("Error while updating changes");
+        }
+
+        public async Task<bool> UpdateTodoDateAsync(TodoItem todoItem)
+        {
+            var item = await TodoItemRepository.GetByAsync(x => x.TodoItemId == todoItem.TodoItemId) ?? throw new Exception("TodoItem not found");
+
+            item.StartDate = todoItem.StartDate;
+            item.EndDate = todoItem.EndDate;
+
+            var result = await TodoItemRepository.UpdateAsync(item);
+            var saveResult = await UnitOfWork.SaveChangesAsync();
+
+            if (saveResult)
+                return saveResult;
+
+            throw new Exception("Error while updating changes");
+        }
+
         public async Task<TodoItem> GetByAsync(Expression<Func<TodoItem, bool>> expression)
             => await TodoItemRepository.GetByAsync(expression);
 
@@ -63,8 +108,39 @@ namespace CQRS.MEDIATOR.V2.API.Services.Implementations
         public async Task<bool> DeleteByAsync(Expression<Func<TodoItem, bool>> expression)
         {
             var todoItem = await TodoItemRepository.GetByAsync(expression) ?? throw new Exception("TodoItem not found");
-            
+
+            todoItem.Deleted = true;
+
+            await TodoItemRepository.UpdateAsync(todoItem);
+            var saveResult = await UnitOfWork.SaveChangesAsync();
+
+            if (saveResult)
+                return saveResult;
+
+            throw new Exception("Error while deleting changes");
+        }
+
+        public async Task<bool> RecoverDeleteAsync(long id)
+        {
+            var todoItem = await TodoItemRepository.GetByAsync(x => x.TodoItemId == id) ?? throw new Exception("TodoItem not found");
+
+            todoItem.Deleted = false;
+
+            await TodoItemRepository.UpdateAsync(todoItem);
+            var saveResult = await UnitOfWork.SaveChangesAsync();
+
+            if (saveResult)
+                return saveResult;
+
+            throw new Exception("Error while recovering changes");
+        }
+
+        public async Task<bool> DeleteRegisterAsync(long id)
+        {
+            var todoItem = await TodoItemRepository.GetByAsync(x => x.TodoItemId == id) ?? throw new Exception("TodoItem not found");
+
             var result = await TodoItemRepository.DeleteByAsync(x => x.TodoItemId == todoItem.TodoItemId);
+
             var saveResult = await UnitOfWork.SaveChangesAsync();
 
             if (saveResult)
